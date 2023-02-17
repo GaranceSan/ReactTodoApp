0f1225082ad10891c61a107e4d213951cf68f626
GaranceSan <garancesan@gmail.com>
2023-02-17 14:01:37 +0100

    travail du matin

--- myreact-todo/src/App.js
+++ myreact-todo/src/App.js
@@ -1,31 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [trucs, setTrucs] = useState([]);
   return (
     <>
-    <div className="app-wrapper">
-      <hi>Liste des trucs urgentissimes : </hi>
-    </div>
-    <Form/>
-    <Todo/>
+      <div className="app-wrapper">
+        <hi>Liste des trucs urgentissimes : </hi>
+      </div>
+      <Form
+        addTrucMuche={(truc) => {
+          setTrucs((prev) => [...prev, truc]);
+        }}
+      />
+      <div className="tasks-list">
+        {trucs.map((truc, index) => (
+          <Todo key={index}>{truc}</Todo>
+        ))}
+      </div>
     </>
   );
 }
 
-const Form = () =>{
-  return <div className="from-wrapper"></div>
-
+function Form({ addTrucMuche }) {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const trucText = e.currentTarget.elements.task.value;
+    addTrucMuche(trucText);
+    console.log({ trucText });
+  };
+  return (
+    <form className="form-wrapper" onSubmit={onSubmit}>
+      <input
+        id="task"
+        className="input"
+        type="text"
+        placeholder="Ajout d'un truc a faire"
+      />
+      <button type="Submit">Envoi</button>
+    </form>
+  );
 }
 
-const Todo = () => {
-  return(<div><Checkbox /></div>)
-
+function Todo() {
+  return (
+    <div>
+      <Checkbox />
+    </div>
+  );
 }
 
-const Checkbox = () => {
-  return <div>
-
-  </div>
+function Checkbox() {
+  const [checked, setChecked] = useState(false);
+  const onChange = (e) => {
+    setChecked(e.target.checked);
+  };
+  return (
+    <div className="checkbox">
+      <input type="checkbox" checked={checked} onChange={onChange} />
+    </div>
+  );
 }
 export default App;
